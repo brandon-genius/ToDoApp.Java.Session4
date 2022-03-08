746f868aa5e184cc4f2e0f4a55f429bf22d89013
Brandon Britt <brandon.wpi@gmail.com>
2022-03-07 19:34:03 -1000

    Just starting
    Just Starting

---
+++ script.js
@@ -0,0 +1,23 @@
+// Simulation
+// Database
+let userName="brandon.wpi@gmail.com";
+let password="Test1234";
+
+//the program
+let Uname = prompt("Enter your username:");
+let pass = prompt("Enter your password:");
+
+if(Uname==userName && pass == password){//true statement
+    console.log("Welcome to the system");
+}else{//false statement
+    console.log("Invalid crudentials");
+}
+
+let number1=prompt("enter first number");
+let number2=prompt("enter second number");
+
+if(number1>number2){
+    console.log("the first number is greater than the second");
+}else{
+    console.log("the first number is either equal to,sdfg or less than the second number");
+}
